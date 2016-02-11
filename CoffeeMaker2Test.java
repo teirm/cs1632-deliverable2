abf5ef72aa3d4f7600e85b7ad33f78c7029f983f
teirm <tribunus.vii@gmail.com>
2016-02-11 07:11:12 -0500

    Added imports for Mockito and Junit

--- CoffeeMaker2Test.java
+++ CoffeeMaker2Test.java
@@ -1,4 +1,10 @@
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 
 public class CoffeeMaker2Test {
 
