1af63ec3b68327bbc4b8426c3459debf786d030f
Evan Sheen <eksheen@gmail.com>
2016-02-11 17:27:15 -0500

    Final room class

--- Room.java
+++ Room.java
@@ -1,4 +1,11 @@
 public class Room {
+	private String roomAdj;
+	private String northDoorAdj;
+	private String southDoorAdj;
+	private String furnitureAdj;
+	private String item;
+
+
 	/* Potential methods: 
 		Constructor (could probably get away with default)
 
@@ -12,4 +19,60 @@ public class Room {
 		GET NORTH DESCRIPTION
 		GET SOUTH DESCRIPTION
 	*/	
+		public Room() {
+
+		}
+
+
+		//setters
+		public void setRoomAdj(String s) {
+			this.roomAdj = s;
+		}
+
+		public void setNorthDoorAdj(String s) {
+			this.northDoorAdj = s;
+		}
+
+		public void setSouthDoorAdj(String s) {
+			this.southDoorAdj = s;
+		}
+
+		public void setFurnitureAdj(String s) {
+			this.furnitureAdj = s;
+		}
+
+		public void setItem(String s) {
+			this.item = s;
+		}
+
+
+		//getters
+		public String getRoomAdj() {
+			return roomAdj;
+		}
+
+		public String getNorthDoorAdj() {
+			return northDoorAdj;
+		}
+
+		public String getSouthDoorAdj() {
+			return southDoorAdj;
+		}
+
+		public String getFurnitureAdj() {
+			return furnitureAdj;
+		}
+
+		Public String getItem() {
+			return item;
+		}
+
+
+
+
+
+
+
+
+
 }
