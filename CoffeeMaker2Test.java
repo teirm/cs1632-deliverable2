3d0fda7746ed17f5e28d86c9fc9b6063107ecf7f
teirm <tribunus.vii@gmail.com>
2016-02-12 15:33:56 -0500

    Wrote dumb method and main for test suite

--- CoffeeMaker2Test.java
+++ CoffeeMaker2Test.java
@@ -1,3 +1,8 @@
+/* Authors: Evan Sheen, Cyrus Ramavarapu
+Date: 12 February 2016
+Purpose: JUnit tests for CoffeMakerQuest2 
+*/
+
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
@@ -5,8 +10,26 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class CoffeeMaker2Test {
 
-	/* HERE BE TESTS */
+
+	// This is a dummy test 
+	@Test
+	public void testEquals() {
+		//Preconditions - a and b reference same object
+		Room a = new Room();
+
+		// Execution steps - reference a from new variable 
+		Room b = a;
+
+		// Postconditions / Expected behavior - assert that
+		// a and b are the same
+		assertSame(a, b);
+	}
+
+
+	// This main method runs our test suite (I think this is a test suite)
+	public static void main(String[] args) {
+		org.junit.runner.JUnitCore.main("CoffeeMaker2Test");
+	}
 }
